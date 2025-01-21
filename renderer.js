b416d2349141c198276953b208db66cbf681f043
Adir <slutzkis@gmail.com>
2025-01-21 16:05:14 +0200

    updaets

--- renderer.js
+++ renderer.js
@@ -150,6 +150,10 @@ function changeColor(e) {
 function changeSpeed() {
     // should toggle between 0.25, 0.5, 1, 1.5, 2, 4.
     // Delete comments when done.
-    window.animSpeed *= 2;
-    alert(window.animSpeed);
+    if (window.animSpeed == 2) {
+        window.animSpeed = 0.25;
+    } else {
+        window.animSpeed *= 2;
+    }
+    
 }
