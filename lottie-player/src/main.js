7b7367abaf9f1c386635d9907c3e9fa468ea08cf
Adir <slutzkis@gmail.com>
2025-04-21 08:54:30 +0300

    Lottie in Tauri

--- lottie-player/src/main.js
+++ lottie-player/src/main.js
@@ -1,18 +1,44 @@
-const { invoke } = window.__TAURI__.core;
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
 
-let greetInputEl;
-let greetMsgEl;
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ lottie-player/src/renderer.js
@@ -0,0 +1,184 @@
+function init() {
+    window.animSpeed = 1;
+    window.count = 0;
+    document.getElementById('fileInput').addEventListener('change', handleFileSelect, false);
+}
+
+function handleFileSelect(event) {
+    const reader = new FileReader()
+    reader.onload = handleFileLoad;
+    reader.readAsText(event.target.files[0])
+}
+
+function handleFileLoad(event) {
+    console.log(event);
+    document.getElementById('fileContent').textContent = event.target.result;
+
+    loadAnim();
+}
+
+function loadAnim() {
+    let button = document.createElement("button");
+    button.num = window.count;
+    button.className = "active";
+    button.paused = 0;
+    document.getElementById('lottie').append(button);
+
+    // document.getElementById('lottie').innerHTML = '';
+
+    document.getElementById('lottie').getElementsByTagName("button")[window.count].setAttribute('filename', document.getElementById('fileInput').files[0].name);
+    if (Math.round((document.getElementById('fileInput').files[0].size) / 1000) < 1001) {
+        document.getElementById('lottie').getElementsByTagName("button")[window.count].setAttribute('filesize', Math.round((document.getElementById('fileInput').files[0].size) / 100) / 10 + 'KB');
+    } else {
+        document.getElementById('lottie').getElementsByTagName("button")[window.count].setAttribute('filesize', Math.round((document.getElementById('fileInput').files[0].size) / 100000) / 10 + 'MB');
+    }
+
+    document.getElementById('lottie').getElementsByTagName("button")[window.count].setAttribute('playing', "true");
+
+    const animationData = JSON.parse(document.getElementById('fileContent').textContent);
+    document.getElementById('lottie').getElementsByTagName("button")[window.count].bodyAnim = bodymovin.loadAnimation({
+        container: document.getElementById('lottie').getElementsByTagName("button")[document.getElementById('lottie').getElementsByTagName("button").length - 1],
+        animationData: animationData,
+        renderer: 'svg',
+        loop: window.loopAll,
+        autoplay: true,
+    });
+
+    document.getElementById('lottie').getElementsByTagName("button")[window.count].bodyAnim.setSpeed(window.animSpeed);
+
+    document.getElementById('lottie').getElementsByTagName('button')[window.count].addEventListener('click', playPause, false);
+    window.count += 1;
+
+    window.lastLastFocus = window.lastFocus;
+    window.lastFocus = document.getElementById('lottie').getElementsByTagName("button")[document.getElementById('lottie').getElementsByTagName("button").length - 1];
+    focusHover();
+}
+
+function playPause() {
+    window.lastLastFocus = window.lastFocus
+    window.lastFocus = document.activeElement;
+    if (document.activeElement.paused !== 1) {
+        document.activeElement.bodyAnim.pause();
+        document.activeElement.paused = 1;
+        document.activeElement.setAttribute('playing', "false");
+    } else {
+        document.activeElement.bodyAnim.play();
+        document.activeElement.paused = 0;
+        document.activeElement.setAttribute('playing', 'true');
+    }
+    focusHover();
+}
+
+function devTools() {
+    mainWindow.webContents.openDevTools();
+}
+
+function focusHover() {
+    var x = document.getElementById("lottie").getElementsByTagName("button");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if (x[i].num == window.lastFocus.num) {
+            x[i].classList.add("focused");
+        } else {
+            x[i].classList.remove("focused");
+        }
+    }
+}
+
+function closeClick() {
+    var x = document.getElementById("lottie").getElementsByTagName("button");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        if (x[i].num == window.lastFocus.num) {
+            x[i].innerText = '';
+            x[i].className = '';
+        }
+    }
+    document.getElementById("fileInput").value = '';
+    if (window.fullScreened == 1) {
+        fullScreenClick();
+    }
+    window.lastFocus = window.lastLastFocus;
+}
+
+function fullScreenClick() {
+    if (window.fullScreened !== 1) {
+        var x = document.getElementById("lottie").getElementsByTagName("button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            if (x[i].num == window.lastFocus.num) {
+                x[i].classList.add('bigger');
+            } else {
+                x[i].classList.remove('bigger');
+            }
+        }
+        window.fullScreened = 1;
+        document.getElementById("fullScreenButton").classList.add('minimize');
+    } else {
+        var x = document.getElementById("lottie").getElementsByTagName("button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].classList.remove('bigger');
+        }
+        window.fullScreened = 0;
+        document.getElementById("fullScreenButton").classList.remove('minimize');
+    }
+}
+
+function playAll() {
+    var x = document.getElementById("lottie").getElementsByTagName("button");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].bodyAnim.goToAndPlay(0);
+        x[i].paused = 0;
+        x[i].setAttribute('playing', ">>");
+    }
+}
+
+function loopClick() {
+    if (window.loopAll == false) {
+        var x = document.getElementById("lottie").getElementsByTagName("button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].bodyAnim.loop = true;
+        }
+        document.getElementById("loopButton").classList.remove("playOnce");
+        window.loopAll = true;
+    } else {
+        var x = document.getElementById("lottie").getElementsByTagName("button");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].bodyAnim.loop = false;
+        }
+        document.getElementById("loopButton").classList.add("playOnce");
+        window.loopAll = false;
+    }
+}
+
+function changeColor(e) {
+    document.querySelector(':root').style.setProperty('--buttonColor', e.target.value);
+}
+
+function changeSpeed() {
+    if (window.animSpeed == 2) {
+        window.animSpeed = 0.5;
+    } else {
+        window.animSpeed *= 2;
+    }
+    var x = document.getElementsByClassName("speed-icon");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].classList.remove('selected');
+    }
+
+    document.getElementById("speed_" + window.animSpeed).classList.add('selected');
+    animationSpeed();
+}
+
+function animationSpeed() {
+    var x = document.getElementById('lottie').getElementsByTagName("button");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].bodyAnim.setSpeed(window.animSpeed);
+    }
+}
